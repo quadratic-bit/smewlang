e124276a23875047379687c7ec12b6e46c345cc3
April <fynjyxbrjd@gmail.com>
2026-09-14 18:03:16 +0300

    style: unprettify functional macros
    I put backslashes neatly in one column, but forgot I use tabs.

--- include/smew/diag.h
+++ include/smew/diag.h
@@ -4,10 +4,10 @@
 #include <smew/buf.h>
 #include <smew/line.h>
 
-#define Diag(Kind)          \
-	struct {            \
-		Kind kind;  \
-		Span span;  \
+#define Diag(Kind) \
+	struct { \
+		Kind kind; \
+		Span span; \
 	}
 
 void print_diag(const char *filename, const SourceBuffer *src, const char *msg, Span span);
--- include/smew/vec.h
+++ include/smew/vec.h
@@ -10,30 +10,30 @@ VecResult _vec_grow_impl(void **buf, size_t *cap,              size_t elem_size,
 void      _vec_init_impl(void **buf, size_t *cap, size_t *len, size_t elem_size, size_t init_cap);
 VecResult _vec_push_impl(void **buf, size_t *cap, size_t *len, size_t elem_size, void  *elem    );
 
-#define Vec(T)               \
-	struct {             \
+#define Vec(T) \
+	struct { \
 		T     *data; \
-		size_t cap;  \
-		size_t len;  \
+		size_t cap; \
+		size_t len; \
 	}
 
-#define vec_free(vec)               \
-	do {                        \
-		free((vec)->data);  \
+#define vec_free(vec) \
+	do { \
+		free((vec)->data); \
 		(vec)->data = NULL; \
-		(vec)->cap  = 0;    \
-		(vec)->len  = 0;    \
+		(vec)->cap  = 0; \
+		(vec)->len  = 0; \
 	} while (0)
 
 
 #define vec_grow(vec, new_size) \
 	_vec_grow_impl((void **)&(vec)->data, &(vec)->cap, sizeof((vec)->data[0]), new_size)
 
-#define vec_init(vec, size)                                                                     \
+#define vec_init(vec, size) \
 	_vec_init_impl((void **)&(vec)->data, &(vec)->cap, &(vec)->len, sizeof((vec)->data[0]), \
 	               size)
 
-#define vec_push(vec, elem)                                                                     \
+#define vec_push(vec, elem) \
 	_vec_push_impl((void **)&(vec)->data, &(vec)->cap, &(vec)->len, sizeof((vec)->data[0]), \
 	               (void *)(elem))
 
