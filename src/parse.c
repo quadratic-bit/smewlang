9175bb7f29957efb12df42d7c4909dbff437fc69
April <fynjyxbrjd@gmail.com>
2026-09-15 12:50:53 +0300

    refactor(parser): split type parser into prefix-postfix with some recovery

--- src/parse.c
+++ src/parse.c
@@ -119,82 +119,94 @@ static AstType *unknown_type(Parser *parser) {
 	return base_type;
 }
 
-static AstType *consume_type(Parser *parser, uint8_t ambient_bp) {
-	AstType *base_type = NULL;
+static AstType *consume_type(Parser *parser, uint8_t ambient_bp);
 
-	const Token *cur_tok;
-	while (1) {
-		cur_tok = parser->cur;
-		if (cur_tok->kind == TOK_LPAREN) {
-			assert(base_type == NULL);
-			consume(parser, TOK_LPAREN);
-			base_type = consume_type(parser, LOWEST_BP);
-			consume(parser, TOK_RPAREN);
-			continue;
-		}
-		if (cur_tok->kind == TOK_AMP) {
-			assert(base_type == NULL);
-			uint8_t bp = get_type_bp(TOK_AMP).right;
-			const Token *amp_tok = parser->cur;
-			consume(parser, TOK_AMP);
-			cur_tok = parser->cur;
-			AstTypeKind borrow_kind = AST_TYPE_BORROW;
-			if (cur_tok->kind == TOK_KEY_MUT) {
-				borrow_kind = AST_TYPE_BORROW_MUT;
-				consume(parser, TOK_KEY_MUT);
-			}
-			AstType *operand = consume_type(parser, bp);
-			base_type = parser_alloc_one(parser, AstType);
-			base_type->kind = borrow_kind;
-			if (borrow_kind == AST_TYPE_BORROW) {
-				base_type->borrow.inner = operand;
-			} else if (borrow_kind == AST_TYPE_BORROW_MUT) {
-				base_type->borrow_mut.inner = operand;
-			} else assert(0);
-			base_type->span = span_span(amp_tok->span, operand->span);
-			continue;
-		}
-		if (cur_tok->kind == TOK_IDENTIFIER) {
-			assert(base_type == NULL);
-			AstIdent *ident = consume_ident(parser);
-			base_type = parser_alloc_one(parser, AstType);
-			base_type->kind = AST_TYPE_NAME;
-			base_type->name.ident = ident;
-			base_type->span = ident->span;
-			continue;
-		}
-		if (cur_tok->kind == TOK_STAR) {
-			assert(base_type != NULL);
-			uint8_t bp = get_type_bp(TOK_STAR).left;
-			if (bp <= ambient_bp) {
-				return base_type;
-			}
-			consume(parser, TOK_STAR);
-			AstType *new_base_type = parser_alloc_one(parser, AstType);
-			new_base_type->kind = AST_TYPE_POINTER;
-			new_base_type->pointer.inner = base_type;
-			new_base_type->span = span_span(base_type->span, cur_tok->span);
-			base_type = new_base_type;
-			continue;
-		}
-		if (cur_tok->kind == TOK_LBRACKET) {
-			assert(base_type != NULL);
-			uint8_t bp = get_type_bp(TOK_LBRACKET).left;
-			if (bp <= ambient_bp) {
-				return base_type;
-			}
-			consume(parser, TOK_LBRACKET);
-			consume(parser, TOK_RBRACKET); // TODO: array_fixed
-			AstType *new_base_type = parser_alloc_one(parser, AstType);
-			new_base_type->kind = AST_TYPE_ARRAY_DYN;
-			new_base_type->array_dyn.inner = base_type;
-			new_base_type->span = span_span(base_type->span, (cur_tok+1)->span);
-			base_type = new_base_type;
-			continue;
-		}
-		break;
+static AstType *consume_type_prefix(Parser *parser) {
+	const Token *cur_tok = parser->cur;
+	if (cur_tok->kind == TOK_LPAREN) {
+		consume(parser, TOK_LPAREN);
+		AstType *base_type = consume_type(parser, LOWEST_BP);
+		consume(parser, TOK_RPAREN);
+		return base_type;
 	}
-	return base_type;
+	if (cur_tok->kind == TOK_AMP) {
+		uint8_t bp = get_type_bp(TOK_AMP).right;
+		const Token *amp_tok = parser->cur;
+		consume(parser, TOK_AMP);
+		cur_tok = parser->cur;
+		AstTypeKind borrow_kind = AST_TYPE_BORROW;
+		if (cur_tok->kind == TOK_KEY_MUT) {
+			borrow_kind = AST_TYPE_BORROW_MUT;
+			consume(parser, TOK_KEY_MUT);
+		}
+		AstType *operand = consume_type(parser, bp);
+		AstType *base_type = parser_alloc_one(parser, AstType);
+		base_type->kind = borrow_kind;
+		if (borrow_kind == AST_TYPE_BORROW) {
+			base_type->borrow.inner = operand;
+		} else if (borrow_kind == AST_TYPE_BORROW_MUT) {
+			base_type->borrow_mut.inner = operand;
+		} else assert(0);
+		base_type->span = span_span(amp_tok->span, operand->span);
+		return base_type;
+	}
+	if (cur_tok->kind == TOK_IDENTIFIER) {
+		AstIdent *ident = consume_ident(parser);
+		AstType *base_type = parser_alloc_one(parser, AstType);
+		base_type->kind = AST_TYPE_NAME;
+		base_type->name.ident = ident;
+		base_type->span = ident->span;
+		return base_type;
+	}
+	return NULL;
+}
+
+static AstType *consume_type_postfix(Parser *parser, AstType *base, uint8_t ambient_bp) {
+	const Token *cur_tok = parser->cur;
+	if (cur_tok->kind == TOK_STAR) {
+		uint8_t bp = get_type_bp(TOK_STAR).left;
+		if (bp <= ambient_bp) {
+			return NULL;
+		}
+		consume(parser, TOK_STAR);
+		AstType *new_base = parser_alloc_one(parser, AstType);
+		new_base->kind = AST_TYPE_POINTER;
+		new_base->pointer.inner = base;
+		new_base->span = span_span(base->span, cur_tok->span);
+		return new_base;
+	}
+	if (cur_tok->kind == TOK_LBRACKET) {
+		uint8_t bp = get_type_bp(TOK_LBRACKET).left;
+		if (bp <= ambient_bp) {
+			return NULL;
+		}
+		consume(parser, TOK_LBRACKET);
+		consume(parser, TOK_RBRACKET); // TODO: array_fixed
+		AstType *new_base = parser_alloc_one(parser, AstType);
+		new_base->kind = AST_TYPE_ARRAY_DYN;
+		new_base->array_dyn.inner = base;
+		new_base->span = span_span(base->span, (cur_tok+1)->span);
+		return new_base;
+	}
+	return NULL;
+}
+
+static AstType *consume_type(Parser *parser, uint8_t ambient_bp) {
+	AstType *base = consume_type_prefix(parser);
+
+	if (base == NULL) {
+		add_diag_expected(parser, AST_DIAG_UNEXPECTED_TOKEN,
+		                  parser->cur->span, "type");
+		return unknown_type(parser);
+	}
+
+	while (1) {
+		AstType *new_base = consume_type_postfix(parser, base, ambient_bp);
+		if (new_base == NULL) break;
+
+		base = new_base;
+	}
+	return base;
 }
 
 static void consume_until_semicolon_or_rbrace(Parser *parser) {
